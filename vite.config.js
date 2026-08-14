896fd39f770c351e1c4080611f00ac7528b6d9a5
CCEMmarinha <daviguarana20@gmail.com>
2026-08-14 13:00:02 -0300

    Permite host do Render no Vite

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: [
+      'radio-marinha-pagina.onrender.com'
+    ],
     proxy: {
       // Proxy para evitar erro de CORS e unificar a origem
       "/api": {
         target: "http://127.0.0.1:3001",
         changeOrigin: true,
         secure: false
-      },
+      }
     }
-  },
+  }
 });
